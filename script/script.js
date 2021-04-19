9343bee324a6f15c6b3abf967f28c9a9b72b1851
wang.xander <wang.xander@gmail.com>
2021-04-19 07:24:56 -0700

    update all

---
+++ script/script.js
@@ -0,0 +1,6 @@
+//alert("i am here");
+document.getElementById("loadOnCardBT").addEventListener("click", loadOnCard);
+
+function loadOnCard() {
+  document.getElementById("imgURL").setAttribute("src", "url");
+}
