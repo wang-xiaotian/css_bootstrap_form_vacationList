5d488d4b334bbccbd5489d968e8134e7b07c16e0
wang.xander <wang.xander@gmail.com>
2021-04-20 09:20:26 -0700

    updated unsplash api

--- script/script.js
+++ script/script.js
@@ -1,6 +1,46 @@
 //alert("i am here");
-document.getElementById("loadOnCardBT").addEventListener("click", loadOnCard);
+const unsplashAPI =
+  "https://api.unsplash.com/search/photos?client_id=CLnWpARpr78PvJHV7Y6ApKdMDkFoxb2eqr_UxKHeO5g&page=1&query=";
 
-function loadOnCard() {
-  document.getElementById("imgURL").setAttribute("src", "url");
+async function query(term) {
+  let api = unsplashAPI + term;
+  try {
+    const res = await fetch(api);
+    const json = await res.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function imgUrl(term) {
+  query(term).then((response) => {
+    let result = response.results[random(10)].urls.large;
+    console.log(result.length);
+    return result;
+  });
+}
+
+document.getElementById("loadOnCardBT").addEventListener("click", (event) => {
+  event.preventDefault();
+  //alert("load on card");
+  let cardImgUrl = document.getElementById("cardImgUrl");
+  let cardText = document.getElementById("card-text");
+  let term = document.getElementById("destination").value;
+  let description = document.getElementById("formDescription").value;
+  if (term.length != 0) {
+    //console.log(term);
+    // document.getElementById("cardImgUrl").setAttribute("src", url);
+    query(term).then((response) => {
+      let images = response.results;
+      let url = images[random(images.length)].urls.thumb;
+      cardImgUrl.setAttribute("src", url);
+      console.log(response.results.length);
+    });
+    cardText.innerHTML = description;
+  }
+});
+
+function random(x) {
+  return Math.floor(Math.random() * x);
 }
